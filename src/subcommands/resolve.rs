8a0f55127f7a887b9619f59121a53285a04cccfc
ktkn304 <12205573+ktkn304@users.noreply.github.com>
2023-06-23 17:21:46 +0900

    add template option to resolve subcommand.

--- src/subcommands/resolve.rs
+++ src/subcommands/resolve.rs
@@ -1,17 +1,19 @@
 use clap::Args;
 use crate::config::Config;
+use crate::config::generator::Generator;
 use crate::store::{EnvironmentStore, Store};
 use crate::utils::{self, GenericResult};
 
 #[derive(Args)]
 pub struct Subcommand {
-    address: String
+    #[clap(short, long)]
+    template: Option<String>,
+    address: String,
 }
 
 impl super::BuiltInCommand for Subcommand {
     fn run(&self, config: &Config) -> GenericResult<i32> {
         let mut store = EnvironmentStore::new();
-        let overload = config.overloads.find_overload_name(&self.address)?;
         let cmdgen = config.shell.compile(&store)?;
         let remote_url = config.parse.parse_url(&self.address)?;
         store.set_remote_raw(&self.address);
@@ -20,7 +22,13 @@ impl super::BuiltInCommand for Subcommand {
         let root_path = config.core.root.expand(&cmdgen, &store)?;
         store.set_root_path(root_path.clone());
 
-        let rel_path = config.resolve.expand_path(&cmdgen, &store, overload)?;
+        let rel_path = if let Some(template) = &self.template {
+            let generator = Generator::Template(template.to_owned());
+            generator.expand(&cmdgen, &store)?
+        } else {
+            let overload = config.overloads.find_overload_name(&self.address)?;
+            config.resolve.expand_path(&cmdgen, &store, overload)?
+        };
         let path = utils::concat_path(&root_path, &rel_path);
         println!("{}", &path);
         return Ok(0);
